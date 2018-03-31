d7ba71337ee5f7a8f97f136185394b9f16fdfdb6
Lara Martin <larikimc@gmail.com>
2018-03-31 20:17:49 +0200

    dispose disposable returned by subscribe()

--- app/src/main/java/eu/laramartin/rxjavaretrofit/MainActivity.kt
+++ app/src/main/java/eu/laramartin/rxjavaretrofit/MainActivity.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+    private var disposable : Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textChangesObservable = RxTextView.textChanges(edit)
-        textChangesObservable.subscribe({
+        disposable = textChangesObservable.subscribe({
             Log.v(TAG, "textview: $it")
         })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }
