2866b2f09f3560815ca4ebd37b518f316d0d1825
Lara Martin <larikimc@gmail.com>
2018-03-31 20:13:17 +0200

    Subscribe to editText changes and log

--- app/src/main/java/eu/laramartin/rxjavaretrofit/MainActivity.kt
+++ app/src/main/java/eu/laramartin/rxjavaretrofit/MainActivity.kt
@@ -2,11 +2,21 @@ package eu.laramartin.rxjavaretrofit
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.jakewharton.rxbinding2.widget.RxTextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textChangesObservable = RxTextView.textChanges(edit)
+        textChangesObservable.subscribe({
+            Log.v(TAG, "textview: $it")
+        })
     }
 }
